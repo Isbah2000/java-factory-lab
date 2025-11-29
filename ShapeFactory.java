52abd76285a46b9fc47010efd4f0e01273d46289
Isbah2000 <k258232@nu.edu.pk>
2025-11-29 19:40:16 +0500

    Create ShapeFactory.java

---
+++ ShapeFactory.java
@@ -0,0 +1,17 @@
+public class ShapeFactory {
+// Simple factory method
+public Shape getShape(String shapeType) {
+if (shapeType == null) return null;
+String key = shapeType.trim().toUpperCase();
+switch (key) {
+case "CIRCLE":
+return new Circle();
+case "SQUARE":
+return new Square();
+case "RECTANGLE":
+return new Rectangle();
+default:
+return null;
+}
+}
+}
